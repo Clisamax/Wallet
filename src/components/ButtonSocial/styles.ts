b0dd433bb00f0cd083ccacd7a81ab9be89e4238b
clisamax <clisamax.gomes.dev@gmail.com>
2023-12-09 00:01:58 -0300

    feat: ButtonSocial contornando erro

---
+++ src/components/ButtonSocial/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+//import { RectButton } from 'react-native-gesture-handler'
+import { RFValue } from 'react-native-responsive-fontsize'
+import theme from '../../../Style/theme'
+
+export const Button = styled.Button`
+  width: ${RFValue(130)}px;
+  height: ${RFValue(60)}px;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${RFValue(5)}px;
+  box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.2);
+
+  background-color: ${theme.COLORS.GRAY4};
+`

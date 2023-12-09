00e464bc9b62469c5b5404e7d3b6c3911435de45
clisamax <clisamax.gomes.dev@gmail.com>
2023-12-08 22:19:36 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback } from 'react'
+
+import { View } from 'react-native'
+
+import theme from './Style/theme'
+
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+
+import { DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+import { Poppins_300Light, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, Poppins_800ExtraBold_Italic } from '@expo-google-fonts/poppins'
+import { Roboto_300Light, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Inter_900Black } from '@expo-google-fonts/inter'
+import { ThemeProvider } from 'styled-components'
+import { StatusBar } from 'expo-status-bar'
+import Login from './src/screens/login'
+
+void SplashScreen.preventAutoHideAsync()
+
+const App: React.FC = () => {
+  const [fontsLoaded] = Font.useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Inter_900Black,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold_Italic,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  const onLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+      <View style={{ flex: 1 }} onLayout={onLayout}>
+        <Login />
+      </View>
+    </ThemeProvider>
+  )
+}
+export default App
---
+++ src/screens/login.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import {
+	Container,
+	ContentHeader,
+	ContentBody,
+	ContentFooter,
+	Title,
+	Description,
+	ViewButton
+} from './style';
+
+const screens: React.FC = () => {
+	return (
+		<SafeAreaView>
+			<Container>
+				<ContentHeader>
+					<Title>Seja Bem Vindo(a) Wallet App</Title>
+					<Description>sign up with</Description>
+					<ViewButton>
+						
+					</ViewButton>
+				</ContentHeader>
+				<ContentBody></ContentBody>
+				<ContentFooter></ContentFooter>
+			</Container>
+		</SafeAreaView>
+	);
+};
+
+export default screens;

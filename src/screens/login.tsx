c5487b9446763de0234bfa261132f679fee74caa
clisamax <clisamax.gomes.dev@gmail.com>
2024-01-14 08:31:35 -0300

    refatorado  login.tsx

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React from 'react'
+import { SafeAreaView } from 'react-native'
 
-import {
-	Container,
-	ContentHeader,
-	ContentBody,
-	ContentFooter,
-	Title,
-	Description,
-	ViewButton
-} from './style';
-import ButtonSocial from '@components/ButtonSocial/ButtonSocial';
+import { Container, ContentHeader, ContentBody, ContentFooter, Title, Description, ViewButton } from './style'
+import ButtonSocial from '@components/ButtonSocial/ButtonSocial'
 
 const screens: React.FC = () => {
-	return (
-		<SafeAreaView>
-			<Container>
-				<ContentHeader>
-					<Title>Seja Bem Vindo(a) Wallet App</Title>
-					<Description>sign up with</Description>
-					<ViewButton>
-						<ButtonSocial/>
-					</ViewButton>
-				</ContentHeader>
-				<ContentBody></ContentBody>
-				<ContentFooter></ContentFooter>
-			</Container>
-		</SafeAreaView>
-	);
-};
+  return (
+    <SafeAreaView>
+      <Container>
+        <ContentHeader>
+          <Title>Seja Bem Vindo(a) Wallet App</Title>
+          <Description>sign up with</Description>
+          <ViewButton>
+            <ButtonSocial />
+          </ViewButton>
+        </ContentHeader>
+        <ContentBody></ContentBody>
+        <ContentFooter></ContentFooter>
+      </Container>
+    </SafeAreaView>
+  )
+}
 
-export default screens;
+export default screens
